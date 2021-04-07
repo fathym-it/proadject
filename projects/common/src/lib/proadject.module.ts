bcc57d659ef25e8018569f366a1c113fe5ec9ac6
powhound4 <george.hatch@fathym.com>
2021-04-07 17:50:42 -0600

    checkpoint

--- projects/common/src/lib/proadject.module.ts
+++ projects/common/src/lib/proadject.module.ts
@@ -3,9 +3,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FathymSharedModule, MaterialModule } from '@lcu/common';
 import { LcuService } from './services/lcu.service';
+import { LcuComponent } from './controls/lcu/lcu.component';
+import { LcuDirective } from './directives/lcu.directive';
+import { ProadjectPointForecastElementComponent } from './elements/point-forecast/point-forecast.component';
+import { ProadjectRouteForecastElementComponent } from './elements/route-forecast/route-forecast.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LcuComponent,
+    LcuDirective,
+    ProadjectPointForecastElementComponent,
+    ProadjectRouteForecastElementComponent
+  ],
   imports: [
     FathymSharedModule,
     FormsModule,
