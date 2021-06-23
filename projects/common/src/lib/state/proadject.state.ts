016e941e9af1ae468a8c9afcdc07831c0f7a95a5
powhound4 <george.hatch@fathym.com>
2021-06-23 09:54:55 -0600

    state

--- projects/common/src/lib/state/proadject.state.ts
+++ projects/common/src/lib/state/proadject.state.ts
@@ -1,4 +1,35 @@
 export class ProadjectState {
 
-  
+    public UsersSettings: UsersSettings;
+
+    public CurrentLocation: Location;
+
+}
+
+export class UsersSettings {
+
+    public FavoriteLocations: Array<Location>;
+
+    public FavoriteRoutes: Array<Route>;
+
+    public Tier: string;
+
+}
+
+export class Location {
+
+    public Latitude: number;
+
+    public Longitude: number;
+
+}
+
+export class Route {
+
+    public Name: string;
+
+    public Points: Array<Location>;
+
+    public Distance: number;
+
 }
